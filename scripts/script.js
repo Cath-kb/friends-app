aadf9496d764b436eca6c2818d46a195678aef1d
Catherine Karnyushina <catherine.nikolaev@gmail.com>
2017-02-19 19:03:57 +0200

    get & show friends from randomuser.me

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const avatar = document.querySelector('.avatar');
 const name = document.querySelector('.name');
 const phone = document.querySelector('.phone');
 const email = document.querySelector('.email');
+const friends = document.getElementById('friends');
 
 let xhr = new XMLHttpRequest();
 
@@ -30,6 +31,24 @@ xhr.onreadystatechange = () => {
                 name.textContent = `${profile.name.first} ${profile.name.last}`;
                 phone.textContent = profile.cell;
                 email.textContent = profile.email;
+
+                xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = () => {
+                    if (xhr.status == 200 && xhr.readyState == 4) {
+                        const response = JSON.parse(xhr.responseText);
+                        const friendList = response.results;
+                        friendList.forEach((friend) => {
+                            const div = document.createElement('div');
+                            const img = document.createElement('img');
+                            div.classList.add('friend');
+                            img.src = friend.picture.large;
+                            div.appendChild(img);
+                            friends.appendChild(div);
+                        });
+                    }
+                };
+                xhr.open('GET', 'https://randomuser.me/api/?results=15', true);
+                xhr.send(null);
             }
         };
         xhr.open('GET', 'https://randomuser.me/api/', true);
