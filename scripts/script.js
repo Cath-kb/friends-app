b743e09b3bc30575e7ab0825aedceb79553e3138
Catherine Karnyushina <catherine.nikolaev@gmail.com>
2017-02-19 18:18:13 +0200

    show photos on client

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,21 @@
+const gallery = document.getElementById('gallery');
+
 const xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
+xhr.onreadystatechange = () => {
     if (xhr.status == 200 && xhr.readyState == 4) {
         const response = JSON.parse(xhr.responseText);
         const photos = response.photos;
-        console.log(photos);
+        photos.forEach((photo) => {
+            const div = document.createElement('div');
+            const img = document.createElement('img');
+
+            div.classList.add('photo');
+            img.src = photo.url;
+
+            div.appendChild(img);
+            gallery.appendChild(div);
+        });
     }
 };
 
