905ddbc7a49465125e3c458b05d37c0baf0e195f
Catherine Karnyushina <catherine.nikolaev@gmail.com>
2017-02-19 18:46:38 +0200

    get & show profile from randomuser.me

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const gallery = document.getElementById('gallery');
+const avatar = document.querySelector('.avatar');
+const name = document.querySelector('.name');
+const phone = document.querySelector('.phone');
+const email = document.querySelector('.email');
 
-const xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = () => {
     if (xhr.status == 200 && xhr.readyState == 4) {
@@ -16,6 +20,20 @@ xhr.onreadystatechange = () => {
             div.appendChild(img);
             gallery.appendChild(div);
         });
+
+        xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.status == 200 && xhr.readyState == 4) {
+                const response = JSON.parse(xhr.responseText);
+                const profile = response.results[0];
+                avatar.src = profile.picture.large;
+                name.textContent = `${profile.name.first} ${profile.name.last}`;
+                phone.textContent = profile.cell;
+                email.textContent = profile.email;
+            }
+        };
+        xhr.open('GET', 'https://randomuser.me/api/', true);
+        xhr.send(null);
     }
 };
 
