332ee254c0b72aac7501f2bcb67da3ee9214b02f
Catherine Karnyushina <catherine.nikolaev@gmail.com>
2017-02-19 19:21:03 +0200

    switch server to express

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 'use strict';
 
-const http = require('http');
-const staticServer = require('node-static');
-const file = new staticServer.Server('.');
+const express = require('express');
+
 const data = require('./data.js');
 
-http.createServer(function(req, res) {
-    if (req.url == '/hello') {
-        res.writeHead(200, {
-            'Content-Type': 'text/plain',
-            'Cache-Control':'no-cache'
-        });
+const port = 3000;
+const app = express();
 
-        res.end('Hi!');
-    } else if (req.url == '/photos') {
-        res.writeHead(200, {
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/photos', (req, res) => {
+    res.set({
             'Content-Type': 'application/json',
             'Cache-Control':'no-cache'
         });
+    res.send( JSON.stringify(data));
+});
 
-        res.end(JSON.stringify(data));
-    } else {
-        file.serve(req, res);
-    }
-}).listen(3000);
-
-console.log('Server running on port 3000');
+app.listen(port);
+console.log(`Server running on port ${port}`);
